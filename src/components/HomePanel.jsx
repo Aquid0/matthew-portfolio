9744ab33dbc0f694bf466aaddec4e36f4de184c8
Matthew <aquidmatthewla@gmail.com>
2025-03-22 05:19:05 +0000

    chore: adding tailwindcss to build

---
+++ src/components/HomePanel.jsx
@@ -0,0 +1,9 @@
+const HomePanel = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">HomePanel</h1>
+        </div>
+    )
+}
+
+export default HomePanel;
