eaa15964e3e5a750c385d9345a089b4f0336acf9
Matthew <aquidmatthewla@gmail.com>
2025-03-23 18:49:49 +0000

    feat: adding temp sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import HomePanel from './components/HomePanel'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <HomePanel />
-      <div className="w-200 h-200 bg-red-500"></div>
+    <div className="flex justify-between gap-50">
+      <div className="w-1/2 flex justify-center items-start pt-20 pl-50">
+        <HomePanel />
+      </div>
+
+      <div className="w-3/4 pt-20">
+        <div className="mb-20">
+          <About />
+        </div>
+        <div className="mb-20">
+          <Experience />
+        </div>
+        <div className="mb-20">
+          <Projects />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">About</h1>
+            <p className="text-lg">
+                I am a software engineer with a passion for building scalable and efficient systems.
+            </p>
+        </div>
+    )
+}
+
+export default About;
