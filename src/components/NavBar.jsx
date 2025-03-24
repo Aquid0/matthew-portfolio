17d5fb3a2d92296fb0a998fa6da0b5e3eed373e4
Matthew <aquidmatthewla@gmail.com>
2025-03-24 03:48:41 +0000

    feat: change font to helvetica

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,33 @@
 const NavBar = () => {
   return (
-    <div class="grid grid-cols-10 gap-0 h-13 border-b border-black uppercase">
-      <div class="relative flex items-center justify-center border-black italic">
+    <div className="grid grid-cols-10 gap-0 h-13 border-b border-black uppercase">
+      <div className="relative flex items-center justify-center border-black italic">
         23:25:00 GMT
         <div className="absolute right-0 top-0 h-screen w-px bg-black" />
       </div>
-      <div class="col-span-2 flex items-center border-black italic gap-10 ml-10">
-        Index
+      <div className="col-span-2 flex items-center border-black italic gap-10 ml-10">
+        <a href="#" className="hover:underline">
+          Index
+        </a>
         <div className="w-px h-7 bg-black"></div>
-        .01 About
+        About
       </div>
-      <div class="col-start-7 flex items-center justify-center border-black italic">
-        .01 About
+      <div className="col-start-7 flex items-center justify-center border-black italic">
+        <a href="#" className="hover:underline">
+          .01 About
+        </a>
       </div>
-      <div class="flex items-center justify-center border-black italic">
-        .02 Experience
+      <div className="flex items-center justify-center border-black italic">
+        <a href="#" className="hover:underline">
+          .02 Experience
+        </a>
       </div>
-      <div class="flex items-center justify-center border-black italic">
-        .03 Projects
+      <div className="flex items-center justify-center border-black italic">
+        <a href="#" className="hover:underline">
+          .03 Projects
+        </a>
       </div>
-      <div class="relative flex items-center justify-center italic">
+      <div className="relative flex items-center justify-center italic">
         <div className="absolute left-0 top-0 h-screen w-px bg-black" />
         Based in UK
       </div>
