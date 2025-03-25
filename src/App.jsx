40e57c597c7fbb489de8c51233ef4c67f4c19a96
Matthew <aquidmatthewla@gmail.com>
2025-03-25 02:44:33 +0000

    svg test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import About from './sections/About';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Header />
+
+      <div className="flex flex-col items-center justify-center mt-40 mr-40">
+        <About />
+      </div>
     </div>
   );
 }
